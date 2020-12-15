2342704322e9b3fbe29ac5b00c92eeea8c7ab431
Kyle Payne <kspayne93@gmail.com>
2020-12-15 10:54:56 -0700

    add fontawesome, classnames, misc styling updates

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,53 +3,53 @@ import styles from './Dashboard.module.scss';
 import globalStyles from '../../styles/globals.module.scss';
 import CategoryRow from '../CategoryRow/CategoryRow';
 
+import classnames from 'classnames';
+
 export default function Dashboard() {
   return (
-    <div className={globalStyles.pageContent}>
-      <div className={styles.dashboard}>
-        <div className={styles.header}>
-          <input type='text' placeholder='Search' className={styles.search} />
+    <div className={classnames(globalStyles.pageContent, styles.dashboard)}>
+      <div className={styles.header}>
+        <input type='text' placeholder='Search' className={styles.search} />
 
-          <div className={styles.companyTitleContainer}>Salt & Straw Creamery</div>
+        <div className={styles.companyTitleContainer}>Salt & Straw Creamery</div>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.leftIndicatorContainer}>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={`${styles.circleIndicator} ${styles.activeIndicator}`}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
+          <div className={styles.circleIndicator}></div>
         </div>
 
-        <div className={styles.main}>
-          <div className={styles.leftIndicatorContainer}>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={`${styles.circleIndicator} ${styles.activeIndicator}`}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
-            <div className={styles.circleIndicator}></div>
+        <div className={styles.roadmapContainer}>
+          <div className={`${styles.rowContainer} ${styles.row1}`}>
+            <CategoryRow title='Defense' />
           </div>
-
-          <div className={styles.rowContainer}>
-            <div className={`${styles.row} ${styles.row1}`}>
-              <CategoryRow title='Defense' />
-            </div>
-            <div className={`${styles.row} ${styles.row2}`}>
-              <CategoryRow title='Compliance' />
-            </div>
-            <div className={`${styles.row} ${styles.row3}`}>
-              <CategoryRow title='Monitoring' isActive={true} />
-            </div>
-            <div className={`${styles.row} ${styles.row4}`}>
-              <CategoryRow title='Governance' />
-            </div>
-            <div className={`${styles.row} ${styles.row5}`}>
-              <CategoryRow title='Product' />
-            </div>
+          <div className={`${styles.rowContainer} ${styles.row2}`}>
+            <CategoryRow title='Compliance' />
+          </div>
+          <div className={`${styles.rowContainer} ${styles.row3}`}>
+            <CategoryRow title='Monitoring' isActive={true} />
+          </div>
+          <div className={`${styles.rowContainer} ${styles.row4}`}>
+            <CategoryRow title='Governance' />
+          </div>
+          <div className={`${styles.rowContainer} ${styles.row5}`}>
+            <CategoryRow title='Product' />
           </div>
         </div>
+      </div>
 
-        <div className={styles.backgroundLayerContainer}>
-          <div className={styles.backgroundLayer1}></div>
-          <div className={styles.backgroundLayer2}></div>
-          <div className={styles.backgroundLayer3}></div>
-        </div>
+      <div className={styles.backgroundLayerContainer}>
+        <div className={styles.backgroundLayer1}></div>
+        <div className={styles.backgroundLayer2}></div>
+        <div className={styles.backgroundLayer3}></div>
       </div>
     </div>
   );
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import styles from './Sidebar.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle, faCommentAlt } from '@fortawesome/free-solid-svg-icons';
+
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <div className={styles.logo}></div>
+      <div className={styles.topContainer}>
+        <div className={styles.logo}></div>
+      </div>
+      <div className={styles.bottomContainer}>
+        <div className={styles.iconContainer}>
+          <FontAwesomeIcon icon={faCommentAlt} />
+          <p className={styles.iconLabel}>Support</p>
+        </div>
+        <div className={styles.iconContainer}>
+          <FontAwesomeIcon icon={faUserCircle} />
+          <p className={styles.iconLabel}>Profile</p>
+        </div>
+      </div>
     </div>
   );
 }
